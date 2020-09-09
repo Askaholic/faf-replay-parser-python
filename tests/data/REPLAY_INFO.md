cce2c4326e8310ca32021ba110b309bf537ba2c8
Askaholic <askaholic907@gmail.com>
2020-09-08 20:43:24 -0800

    Add test data

---
+++ tests/data/REPLAY_INFO.md
@@ -0,0 +1,8 @@
+# Replay Info
+
+## 6176549 (Valid)
+- Setons Clutch
+
+## 1418712 (Invalid)
+Corrupted at a size field meaning the replay should error out with "Requested
+size too large!".
---
+++ tests/test_module.py
@@ -0,0 +1,9 @@
+import fafreplay
+
+
+def test_available_objects():
+    assert fafreplay.Parser
+    assert fafreplay.ReplayReadError
+    assert fafreplay.ReplayDesyncedError
+    assert fafreplay.body_offset
+    assert fafreplay.body_ticks
