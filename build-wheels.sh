1fe0bf9d530497383731e49d8066f29e2a3feb70
Askaholic <askaholic907@gmail.com>
2021-04-24 18:04:55 -0800

    Remove python 3.5

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,7 +4,7 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
-for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
