126cbedb51b963b4e554c65afcc405e87c6ac066
Askaholic <askaholic907@gmail.com>
2021-04-24 22:20:06 -0800

    Clean up previously compiled objects
    Leaving them causes them to be included in the next wheel

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,7 +4,10 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
+mkdir build
+
 for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
+    rm -f build/*
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
