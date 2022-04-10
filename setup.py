73a377f0412760524a29bb622adc5c2d5c8ea5b1
Askaholic <askaholic@protonmail.com>
2022-04-10 10:19:12 -0800

    Switch pipenv for tox

--- setup.py
+++ setup.py
@@ -9,23 +9,12 @@ with open("Cargo.toml") as f:
 package = cargo["package"]
 
 setup(
-    name="faf-replay-parser",
     version=package["version"],
     author=re.match(r"(\w+).*(<.*>)?", package["authors"][0]).group(1),
     description=package["description"],
-    long_description=open("README.md").read(),
-    long_description_content_type='text/markdown',
-    license="License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-    url="https://github.com/Askaholic/faf-replay-parser-python",
     rust_extensions=[RustExtension(
         cargo["lib"]["name"],
         binding=Binding.PyO3
     )],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: POSIX :: Linux",
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
-    ],
     zip_safe=False,
 )
